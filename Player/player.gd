78b6abd2b1ac7cf74d837729febad2e89aca97b4
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-13 14:15:35 +0200

    add minimum jump

--- Player/player.gd
+++ Player/player.gd
@@ -22,18 +22,27 @@ func _physics_process(delta):
 		loading_up = true
 		jump_meter += 2
 		print(jump_meter)
+		
 	elif loading_up == true or jump_meter >= 100:
 		if Input.is_action_pressed("move_right"): 
 			velocity.x += speed * 2
 		if Input.is_action_pressed("move_left"): 
 			velocity.x -= speed * 2
-		velocity.y -= (jump_meter * 5)
+		
+		if jump_meter < 20:
+			velocity.y -= (20 * 5)
+		else:
+			velocity.y -= (jump_meter * 5)
 		loading_up = false
 		jump_meter = 0;
 		can_jump = false
 	
 	velocity.y += 10
 	
+	var pre_bounce_velocity_x = velocity.x
+	
 	move_and_slide()
 	
-	
+	if is_on_wall() and not is_on_floor():
+		velocity.x = -pre_bounce_velocity_x * 0.5
+		velocity.y = velocity.y / 2
