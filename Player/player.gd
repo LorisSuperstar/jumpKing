421d9db7385baa454200d45153d98ae42c166b3c
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-13 13:07:27 +0200

    add basic jump

--- Player/player.gd
+++ Player/player.gd
@@ -1,18 +1,24 @@
 extends CharacterBody2D
 
 @export var speed = 400
-
+var can_jump = true
 	
 func _physics_process(delta):
+	if is_on_floor():
+		velocity.y = 0
+		can_jump = true
 	velocity.x = 0
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= speed
+	if Input.is_action_pressed("jump"):
+		if can_jump == true:
+			can_jump = false
+			velocity.y -= 300
 	
 	velocity.y += 10
 	
 	move_and_slide()
 	
-	if is_on_floor():
-		velocity.y = 0
+	
