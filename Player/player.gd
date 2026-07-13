096baa8c55c4f3096ce9f4d8e81f4071c99216e5
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-13 15:42:58 +0200

    add cool character

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,17 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@onready var animated_sprite = $AnimatedSprite2D
+
 var can_jump = true
 var jump_meter = 0
 var loading_up = false
 	
 func _physics_process(delta):
 	
+	
 	if (is_on_floor()): 
+		animated_sprite.play("Idle")
 		can_jump = true
 		velocity.x = 0
 	else: 
@@ -16,8 +20,10 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_right") and loading_up == false and can_jump == true:
 		velocity.x += speed
+		animated_sprite.flip_h = false
 	if Input.is_action_pressed("move_left") and loading_up == false and can_jump == true:
 		velocity.x -= speed
+		animated_sprite.flip_h = true
 	if Input.is_action_pressed("jump") and can_jump == true and jump_meter < 100:
 		loading_up = true
 		jump_meter += 2
@@ -25,8 +31,10 @@ func _physics_process(delta):
 		
 	elif loading_up == true or jump_meter >= 100:
 		if Input.is_action_pressed("move_right"): 
+			animated_sprite.flip_h = false
 			velocity.x += speed * 2
 		if Input.is_action_pressed("move_left"): 
+			animated_sprite.flip_h = true
 			velocity.x -= speed * 2
 		
 		if jump_meter < 20:
@@ -44,5 +52,7 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	if is_on_wall() and not is_on_floor():
-		velocity.x = -pre_bounce_velocity_x * 0.5
+		velocity.x = -pre_bounce_velocity_x / 3
 		velocity.y = velocity.y / 1.5
+	if is_on_ceiling():
+		velocity.x = velocity.x / 2
