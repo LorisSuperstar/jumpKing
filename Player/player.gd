bbe0edb26baf1e6e4a88c312678a7cccafad6ae9
LorisSuperstar <180442737+LorisSuperstar@users.noreply.github.com>
2026-07-13 13:57:10 +0200

    add jump king jump

--- Player/player.gd
+++ Player/player.gd
@@ -1,21 +1,36 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 100
 var can_jump = true
+var jump_meter = 0
+var loading_up = false
 	
 func _physics_process(delta):
-	if is_on_floor():
-		velocity.y = 0
+	
+	if (is_on_floor()): 
 		can_jump = true
-	velocity.x = 0
-	if Input.is_action_pressed("move_right"):
+		velocity.x = 0
+	else: 
+		can_jump = false
+		
+	
+	if Input.is_action_pressed("move_right") and loading_up == false and can_jump == true:
 		velocity.x += speed
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("move_left") and loading_up == false and can_jump == true:
 		velocity.x -= speed
-	if Input.is_action_pressed("jump"):
-		if can_jump == true:
-			can_jump = false
-			velocity.y -= 300
+	if Input.is_action_pressed("jump") and can_jump == true and jump_meter < 100:
+		loading_up = true
+		jump_meter += 2
+		print(jump_meter)
+	elif loading_up == true or jump_meter >= 100:
+		if Input.is_action_pressed("move_right"): 
+			velocity.x += speed * 2
+		if Input.is_action_pressed("move_left"): 
+			velocity.x -= speed * 2
+		velocity.y -= (jump_meter * 5)
+		loading_up = false
+		jump_meter = 0;
+		can_jump = false
 	
 	velocity.y += 10
 	
